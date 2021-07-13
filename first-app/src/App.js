696927b7c6b1db100a33d0e6d5052ad17cd025f9
VahePashayan <vpashayan39gmail.com>
2021-07-13 14:46:10 +0400

    feat: created react app

---
+++ first-app/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
+const weatherInfo = [
+  {
+    weekDey: "Monday",
+    imgURL: "",
+    temp: "36C"
+  }
+  {
+    weekDey: "Tuesday",
+    imgURL: "",
+    temp: "32C"
+  }
+  {
+    weekDey: "Wednesday",
+    imgURL: "",
+    temp: "35C"
+  }
+{
+    weekDey: "Thursday",
+    imgURL: "",
+    temp: "45C"
+}
+{
+    weekDey: "Friday",
+    imgURL: "",
+    temp: "36C"
+  }
+{
+    weekDey: "Saturday",
+    imgURL: ""
+temp: "37C"
+  }
+{
+    weekDey: "Sunday",
+    imgURL: ""
+temp: "39C"
+  }
+]
